c9905fb460eab458095bf8ebb14df32e49943984
marianylund <marianylund@pm.me>
2020-08-29 10:47:35 +0200

    Deleted hello world, added side bar with profile image

---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+
+      <v-navigation-drawer
+        v-model="drawer"
+        app 
+        dark
+        permanent
+      >
+        <v-list
+          nav
+        >
+        
+          <v-list-item>
+            <v-list-item-avatar class="outlinedAvatar" size=150>
+              <img src="../assets/profile.jpg">
+            </v-list-item-avatar>
+           
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+  </v-container>
+</template>
+
+<style scoped>
+.outlinedAvatar {
+    border-radius: 50%;
+    margin-left: auto !important;
+    margin-right: auto !important;
+    box-shadow: 0px 0px 0px 10px rgb(78, 71, 71);
+}
+</style>
